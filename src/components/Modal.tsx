a8272d25da1daa12a42cf4a2aec50e136b2f60e6
Nicolashh <nicoo2003@gmail.com>
2025-05-21 12:31:31 -0300

    change parameters of the Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,17 @@
-import { FeedBack } from "../types/word.type"
+import { useEffect } from "react"
+import { Word } from "../types/word.type"
 
-export const Modal = ({word, feedbacks}:{word:string, feedbacks:FeedBack[][]}) => {
+export const Modal = ({words}:{words:Word[]}) => {
+
+  useEffect(()=>{
+    console.log(words)
+  }, [])
+
   return (
     <div className="z-10 absolute top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="w-70 h-50 flex flex-col pt-3 items-center bg-gray-700 rounded-3xl">
             <h2 className="text-white text-2xl">GANASTE!</h2>
-            <span className="text-white text-2xl mt-2">{word}</span>
+            <span className="text-white text-2xl mt-2">Hola</span>
 
             <div>
 
