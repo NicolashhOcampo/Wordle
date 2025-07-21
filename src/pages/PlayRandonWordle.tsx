f6943a7dde2c8c6937524a596cfa0bf6110c68c5
Nicolashh <nicoo2003@gmail.com>
2025-07-21 15:09:34 -0300

    refactor: integrate formatWord utility for better word formatting and enhance regex for alphabet validation

--- src/pages/PlayRandonWordle.tsx
+++ src/pages/PlayRandonWordle.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useMemo, useState } from "react"
 import { Wordle } from "../classes/Wordle";
 import { PlayWordle } from "../components/PlayWordle";
+import { formatWord } from "../utils/stringUtils";
 
-function PlayRandomWordle() {
+export const PlayRandomWordle = () => {
   const [secretWord, setSecretWord] = useState("");
   const wordle = useMemo(() => new Wordle(secretWord), [secretWord]);
 
   useEffect(()=>{
     fetch("https://random-word-api.herokuapp.com/word?length=5&lang=ES")
     .then(res => res.json())
-    .then(([data]) => setSecretWord(data))
-    .catch(e => {console.log(e), setSecretWord("error")})
+    .then(([data]) => setSecretWord(formatWord(data)))
+    .catch(e => {console.log(e); setSecretWord("error")})
   }, [])
 
 
